341708a8a2af82c458b9bc03379fb887f4b76dd9
Nicodetonado <nicolass159753@hotmail.com>
2023-10-05 19:38:48 -0300

    coisas de menu de navegação

---
+++ app/src/main/java/br/com/etecia/myapp/MenuBottomActivity2.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuBottomActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_bottom_layout2);
+    }
+}
